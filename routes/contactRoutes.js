4ca596b6e798cad5d4330efbcb8452bf07407fa9
rsingad <r.singerjat@gmail.com>
2025-05-29 02:09:26 +0530

    GET CONTECTS

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,9 @@
 // routes/contactRoutes.js
 const express = require("express");
 const router = express.Router();
-const { submitContactForm } = require("../controllers/contactController");
+const { submitContactForm, getAllContacts } = require("../controllers/contactController");
 
 router.post("/", submitContactForm);
+router.get("/", getAllContacts);
 
 module.exports = router;
